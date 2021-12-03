60f0d63c48ea615ec2d448f0c2e3748b9177b0e6
Schehat Abdel Kader <schehat2000@live.de>
2021-12-03 15:34:36 +0100

    changed folder structure

--- prog_proj3/CONSTRUCT/game_launcher.sh
+++ prog_proj3/CONSTRUCT/game_launcher.sh
@@ -5,6 +5,10 @@
 # Das Skript erwartet als Eingabeargument eine Zahl, welcher für ein Spiel steht
 # z.B.: ./game_launcher.sh 1
 
+# Bedienungsanleitung: 
+
+# Für die Benutung von Spiel 1 ist die java jdk 1.8 notwendig  
+
 # Hier die Liste der Spieler mit den dazugehörigen Ziffern
 # 1: Construct - ist ein Klon des Spiels World Hardest Game, welcher im Matrix Universum spielt 
 
@@ -19,9 +23,15 @@ then
     echo 'bitte geben Sie als Paramter eine Zahl von 1 bis 6 an';
 	exit 1;
 fi
+
 num=$1;
 
 if [ $1 -eq 1 ]
 then
     java -jar Construct.jar
+fi 
+
+if [ $1 -eq 2 ]
+then
+    java -jar THWG.jar
 fi 
