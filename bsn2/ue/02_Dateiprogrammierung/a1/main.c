46434c0034f341a25ee844a0137248e6738b7818
schehat <schehat2000@live.de>
2022-03-19 20:21:59 +0100

    bsn2 ue02 updated

--- bsn2/ue/02_Dateiprogrammierung/a1/main.c
+++ bsn2/ue/02_Dateiprogrammierung/a1/main.c
@@ -6,10 +6,24 @@
 
 int main(void)
 {
-    int fd = open("test.txt", O_RDONLY);
+    char file[] = "test.txt";
+    printf("%s %s\n", "Öffne Datei", file);
+    int fd = open(file, O_RDONLY);
+    if (fd <= 0)
+    {
+        printf("%s\n", "Öffnen der Datei ist fehlgeschlagen");
+        return -1;
+    }
     // new line important due to terminal line buffered
-    printf("%d\n", fd);
+    printf("%s %d\n", "file descriptor: ", fd);
     // alternative if not line buffered e.g. file then this
     // fflush(STDOUT_FILENO)
+
+    // schlafe 1min
+    printf("%s\n", "schlafe 1min");
     sleep(60);
+
+    close(fd);
+
+    return 0;
 }
---
+++ bsn2/ue/02_Dateiprogrammierung/a4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void)
+{
+    char file[] = "test.txt";
+    int fd = open(file, O_RDONLY);
+    int curr_pos = lseek(fd, 0, SEEK_END);
+    printf("%s %d", "Aktuelle Position des Zugriffzeigers:", curr_pos);
+    close(fd);
+}
