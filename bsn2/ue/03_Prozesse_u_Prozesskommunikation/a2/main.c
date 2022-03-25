78f1e62f49dbfd0df684e0da49b82ecda0a983a6
schehat <schehat2000@live.de>
2022-03-25 18:07:10 +0100

    bsn2 ue03 updated

--- bsn2/ue/03_Prozesse_u_Prozesskommunikation/a2/main.c
+++ bsn2/ue/03_Prozesse_u_Prozesskommunikation/a2/main.c
@@ -5,7 +5,7 @@
 #include <sys/wait.h>
 #define SIZE 50
 
-int createChild(int counter)
+int createChildRecursive(int counter)
 {
     int wait_status;
     pid_t pid;
@@ -44,6 +44,43 @@ int createChild(int counter)
 
 int main(void)
 {
-    createChild(0);
+    // createChildRecursive(0);
+    int wait_status;
+    pid_t pid;
+    for (int i = 0; i < SIZE; ++i)
+    {
+        pid = fork();
+        if (pid == -1)
+        {
+            printf("%s", "Fehler beim forken!");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            // last child prints
+            if (i == SIZE - 1)
+            {
+                printf("%s %i\t %s %i\t %s %i\n", "Kind: fork() =", pid, "PID =", getpid(), " PPID =", getppid());
+                char c;
+                scanf("%c", &c);
+            }
+        }
+        else
+        {
+            // wait for child
+            waitpid(pid, &wait_status, 0);
+            // first parent printing at the end. i == 0 being skipped by childs
+            if (i == 0)
+            {
+                printf("%s %i\t %s %i\t %s %i\n", "Eltern: fork() =", pid, "PID =", getpid(), " PPID =", getppid());
+            }
+            else
+            {
+                // childs who got childs end up being parent needed to get printed
+                printf("%s %i\t %s %i\t %s %i\n", "Kind: fork() =", pid, "PID =", getpid(), " PPID =", getppid());
+            }
+            break;
+        }
+    }
     return EXIT_SUCCESS;
 }
