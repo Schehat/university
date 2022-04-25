0e3f732967e3692d85a602d6bcabd7d91bff4b61
schehat <schehat2000@live.de>
2022-04-25 21:21:15 +0200

    web pue updated

---
+++ 4_semester/web/pflichtaufgaben/steam/src/main/java/de/hsh/steam/resources/UserResource.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+import de.hsh.steam.entities.User;
+import de.hsh.steam.repositories.SerializedSeriesRepository;
+import de.hsh.steam.services.SteamService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("users")
+public class UserResource {
+
+    @Inject
+    SteamService steamService;
+    @Inject
+    SerializedSeriesRepository serializedSeriesRepository;
+
+    @GET
+    public Response listAllUsers() {
+        List<User> users = serializedSeriesRepository.getAllUsers();
+        if (users == null) {
+            return Response.status(404).build();
+        } else {
+            return Response.ok().entity(users).build();
+        }
+    }
+
+}
