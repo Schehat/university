9ee731a8a3cf2e5cdc36760cf1a6c6504a67a600
schehat <schehat2000@live.de>
2022-04-28 08:28:01 +0200

    web pue test

--- 4_semester/web/pflichtaufgaben/steam/src/main/java/de/hsh/steam/resources/UserResource.java
+++ 4_semester/web/pflichtaufgaben/steam/src/main/java/de/hsh/steam/resources/UserResource.java
@@ -1,3 +1,5 @@
+package de.hsh.steam.resources;
+
 import java.util.List;
 
 import de.hsh.steam.entities.User;
@@ -5,8 +7,13 @@ import de.hsh.steam.repositories.SerializedSeriesRepository;
 import de.hsh.steam.services.SteamService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
 
 @Path("users")
 public class UserResource {
@@ -22,7 +29,37 @@ public class UserResource {
         if (users == null) {
             return Response.status(404).build();
         } else {
-            return Response.status(200).entity(users).build();
+            return Response.ok().entity(users).build();
         }
     }
+
+    @GET
+    @Path("/{name}")
+    public Response get(@PathParam("name") String name) {
+        User user = this.serializedSeriesRepository.getUserObject(name);
+        if (user == null) {
+            return Response.status(404).build();
+        } else {
+            return Response.ok().entity(user).build();
+        }
+    }
+
+    @POST
+    public Response create(User user, @Context UriInfo uriInfo) {
+        if (steamService.newUser(user.getUsername(), user.getPassword())) {
+            return Response.status(409).build();
+        } else {
+            this.serializedSeriesRepository.registerUser(user);
+            UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+            uriBuilder.path(user.getUsername());
+            user = this.serializedSeriesRepository.getUserObject(user.getUsername());
+            return Response.created(uriBuilder.build()).entity(user).build();
+        }
+    }
+
+    @Path("/{name}/ratings")
+    public RatingResource getRatingResource() {
+        return new RatingResource();
+    }
+
 }
