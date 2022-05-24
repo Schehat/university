86c83e2bcd6f5f6e9e210d6f726e7d72135ac2ed
schehat <schehat2000@live.de>
2022-05-24 16:49:46 +0200

    cg01 ue05 finished chair and man

--- cg1/test/Source/CgUtils/Functions.h
+++ cg1/test/Source/CgUtils/Functions.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -21,25 +20,3 @@ private:
 };
 
 #endif // FUNCTIONS_H
-=======
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include <glm/glm.hpp>
-#include <vector>
-
-class Functions
-{
-public:
-    static std::vector<glm::vec3> Lane_Riesenfeld_Unterteilungs_Algorithmus(std::vector<glm::vec3> vertices, int n);
-    static int getId();
-    static bool XOR(bool a, bool b);
-
-
-private:
-    static int unique_id;
-
-};
-
-#endif // FUNCTIONS_H
->>>>>>> 2bba44709ff951a034d04fd1e87594f07d0ef76e
